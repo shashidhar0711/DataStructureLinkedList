eba7301dee7e558cfbf68435c41781f8418151f7
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-07 00:09:52 +0530

    [sasic] Add . Deleting Elements at Rear End in Linked List

--- DataStructureLinkedList/Program.cs
+++ DataStructureLinkedList/Program.cs
@@ -4,9 +4,27 @@ namespace DataStructureLinkedList
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Linked List");
+            LinkedList linkedList = new LinkedList();
+
+            /// Adding elements into linkedlist and, 
+            /// printing all elements
+            linkedList.AddingElementsAtFrontEnd(70);
+            linkedList.AddingElementsAtFrontEnd(30);
+            linkedList.AddingElementsAtFrontEnd(56);
+            linkedList.Display();
+
+            /// Elements are deleted at rear end
+            /// printing all elements
+            linkedList.DeleteAtRearEnd();
+            linkedList.Display();
+
         }
     }
 }
