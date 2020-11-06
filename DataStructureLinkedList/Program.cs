7d544cc42b9525239ab16965a46bee93ae41d571
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-06 21:38:28 +0530

    Add project files.

---
+++ DataStructureLinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
