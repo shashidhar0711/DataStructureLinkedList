636a7c3b24044de271a17f5dc79df850a4f5ffb7
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-06 22:00:19 +0530

    [sasic] Add . Creating Simple Linked List

---
+++ DataStructureLinkedList/LinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureLinkedList
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        /// <summary>
+        /// Adds the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node Temp = head;
+
+                while (Temp.next != null)
+                {
+                    Temp = Temp.next;
+                }
+
+                Temp.next = node;
+            }
+            Console.WriteLine("{0} inserted in to linked list " + node.data);
+        }
+
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        internal void Display()
+        {
+            Node temp = this.head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("linkedlist is emty");
+                return;
+            }
+
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructureLinkedList/Program.cs
+++ DataStructureLinkedList/Program.cs
@@ -4,9 +4,19 @@ namespace DataStructureLinkedList
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Linked List");
+            LinkedList linkedList = new LinkedList();
+            /// Adding elements into linkedlist and printing all
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
         }
     }
 }
