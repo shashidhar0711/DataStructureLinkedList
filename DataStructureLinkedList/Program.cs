db2d6368e342600cc43af2a331b447182b48ba6e
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-06 23:52:48 +0530

    [sasic] Add . Deleting the Elements at Front End in Linked List

--- DataStructureLinkedList/Program.cs
+++ DataStructureLinkedList/Program.cs
@@ -2,11 +2,27 @@
 
 namespace DataStructureLinkedList
 {
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to linked list");
+
+            LinkedList linkedList = new LinkedList();
+
+            /// Insert elements at end
+            linkedList.InsertElemetAtEnd(56);
+            linkedList.InsertElemetAtEnd(30);
+            linkedList.InsertElemetAtEnd(70);
+
+            /// Deleting the element at front end
+            /// printing all element
+            linkedList.DeleteAtFrontEnd();
+            linkedList.Display();
         }
     }
 }
