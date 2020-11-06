fb1a93f8815ed72220ebc7fcebf3cbe67f67e774
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-06 22:57:20 +0530

    [sasic] Add . Inserting Elements at End Position in Linked List

---
+++ DataStructureLinkedList/LinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureLinkedList
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        /// <summary>
+        /// Adds the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        internal void InsertNodeAtEnd(int data)
+        {
+            /// Creating reference of objec class
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node Temp = head;
+
+                while (Temp.next != null)
+                {
+                    Temp = Temp.next;
+                }
+
+                Temp.next = node;
+            }
+            Console.WriteLine("{0} inserted in to linked list " + node.data);
+        }
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        internal void Display()
+        {
+            Node temp = this.head;
+
+            /// If temp is null ,it gives linked is empty
+            /// If not, it will prints upto condition meets
+            if (temp == null)
+            {
+                Console.WriteLine("linkedlist is empty");
+                return;
+            }
+
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructureLinkedList/Program.cs
+++ DataStructureLinkedList/Program.cs
@@ -2,11 +2,24 @@
 
 namespace DataStructureLinkedList
 {
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Linked List");
+            LinkedList linkedList = new LinkedList();
+
+            /// Insert elements at position end 
+            linkedList.InsertNodeAtEnd(56);
+            linkedList.InsertNodeAtEnd(30);
+            linkedList.InsertNodeAtEnd(70);
+
+            /// Printing all the elements
+            linkedList.Display();
         }
     }
 }
