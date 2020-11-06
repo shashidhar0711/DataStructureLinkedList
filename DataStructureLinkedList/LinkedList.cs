7806b5664dd010f40b6757e2227369b9888b49e2
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-06 22:28:21 +0530

    [sasic] Add . Adding Elements At Font End in Linked List

---
+++ DataStructureLinkedList/LinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureLinkedList
+{
+    class LinkedList
+    {
+        internal Node head;
+
+        internal void AddingElementsAtFrontEnd(int data)
+        {
+            /// Creating refrence of object class
+            Node node = new Node(data);
+
+            /// if head is null creating a new node
+            if (head == null)
+            {
+                head = new Node(data);
+                return;
+            }
+
+            /// if not, let node.next point to the same place where head is pointing
+            /// then head is pointing where  node is pointing.
+            node.next = head;
+            head = node;
+        }
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        internal void Display()
+        {
+            Node temp = this.head;
+
+            /// If temp is null ,it gives linked is empty
+            /// If not, it will prints upto condition meets
+            if (temp == null)
+            {
+                Console.WriteLine("linkedlist is empty");
+                return;
+            }
+
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructureLinkedList/Program.cs
+++ DataStructureLinkedList/Program.cs
@@ -4,9 +4,20 @@ namespace DataStructureLinkedList
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Linked List");
+            LinkedList linkedList = new LinkedList();
+            /// Adding elements into linkedlist and 
+            /// printing all elements
+            linkedList.AddingElementsAtFrontEnd(70);
+            linkedList.AddingElementsAtFrontEnd(30);
+            linkedList.AddingElementsAtFrontEnd(56);
+            linkedList.Display();
         }
     }
 }
